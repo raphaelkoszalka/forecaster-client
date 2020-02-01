8544a977207f73f43b47fb9959838f613e4bc3ee
Raphael Luiz Martins Koszalka <rmkoszalka@gmail.com>
2020-02-01 14:37:11 -0300

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CurrentDayComponent } from './components/current-day/current-day.component';
+import { FiveDayForecastComponent } from './components/five-day-forecast/five-day-forecast.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CurrentDayComponent,
+    FiveDayForecastComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/current-day/current-day.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-current-day',
+  templateUrl: './current-day.component.html',
+  styleUrls: ['./current-day.component.scss']
+})
+export class CurrentDayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
